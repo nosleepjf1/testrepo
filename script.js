fcce4c30f2897c62eaeb410be1988eec0623f11d
jonny fewkes <jonnyfewkes@jonnys-Mac-mini.local>
2017-09-20 16:50:29 -0600

    all rows built but the logic error is still there

--- script.js
+++ script.js
@@ -46,53 +46,168 @@ var block5num3 = ["block4num1","block4num2","block4num3","block5num1","block5num
 var block6num1 = ["block4num1","block4num2","block4num3","block5num1","block5num2","block5num3","block3num1","block3num4","block3num7"];
 var block6num2 = ["block4num1","block4num2","block4num3","block5num1","block5num2","block5num3","block6num1","block3num2","block3num5","block3num8"];
 var block6num3 = ["block4num1","block4num2","block4num3","block5num1","block5num2","block5num3","block6num1","block6num2","block3num3","block3num6","block3num9"];
+//step six
+var block4num4 = ["block1num1","block1num4","block1num7","block4num1"];
+var block4num5 = ["block4num4","block1num2","block1num5","block1num8","block4num2"];
+var block4num6 = ["block4num4","block4num5","block1num3","block1num6","block1num9","block4num3"];
+var block5num4 = ["block4num4","block4num5","block4num6","block2num1","block2num4","block2num7","block5num1"];
+var block5num5 = ["block4num4","block4num5","block4num6","block5num4","block2num2","block2num5","block2num8","block5num2"];
+var block5num6 = ["block4num4","block4num5","block4num6","block5num4","block5num5","block2num3","block2num6","block2num9","block5num3"];
+var block6num4 = ["block4num4","block4num5","block4num6","block5num4","block5num5","block5num6","block3num1","block3num4","block3num7","block6num1"];
+var block6num5 = ["block4num4","block4num5","block4num6","block5num4","block5num5","block5num6","block6num4","block3num2","block3num5","block3num8","block6num2"];
+var block6num6 = ["block4num4","block4num5","block4num6","block5num4","block5num5","block5num6","block6num4","block6num5","block3num3","block3num6","block3num9","block6num3"];
+//step seven
+var block4num7 = ["block1num1","block1num4","block1num7","block4num1","block4num4"];
+var block4num8 = ["block4num7","block1num2","block1num5","block1num8","block4num2","block4num5"];
+var block4num9 = ["block4num7","block4num8","block1num3","block1num6","block1num9","block4num3","block4num6"];
+var block5num7 = ["block4num7","block4num8","block4num9","block2num1","block2num4","block2num7","block5num1","block5num4"];
+var block5num8 = ["block4num7","block4num8","block4num9","block5num7","block2num2","block2num5","block2num8","block5num2","block5num5"];
+var block5num9 = ["block4num7","block4num8","block4num9","block5num7","block5num8","block2num3","block2num6","block2num9","block5num3","block5num6"];
+var block6num7 = ["block4num7","block4num8","block4num9","block5num7","block5num8","block5num9","block3num1","block3num4","block3num7","block6num1","block6num4"];
+var block6num8 = ["block4num7","block4num8","block4num9","block5num7","block5num8","block5num9","block6num7","block3num2","block3num5","block3num8","block6num2","block6num5"];
+var block6num9 = ["block4num7","block4num8","block4num9","block5num7","block5num8","block5num9","block6num7","block6num8","block3num3","block3num6","block3num9","block6num3","block6num6"];
+//step eight
+var block7num1 = ["block1num1","block1num4","block1num7","block4num1","block4num4","block4num7"];
+var block7num2 = ["block7num1","block1num2","block1num5","block1num8","block4num2","block4num5","block4num8"];
+var block7num3 = ["block7num1","block7num2","block1num3","block1num6","block1num9","block4num3","block4num6","block4num9"];
+var block8num1 = ["block7num1","block7num2","block7num3","block2num1","block2num4","block2num7","block5num1","block5num4","block5num7"];
+var block8num2 = ["block7num1","block7num2","block7num3","block8num1","block2num2","block2num5","block2num8","block5num2","block5num5","block5num8"];
+var block8num3 = ["block7num1","block7num2","block7num3","block8num1","block8num2","block2num3","block2num6","block2num9","block5num3","block5num6","block5num9"];
+var block9num1 = ["block7num1","block7num2","block7num3","block8num1","block8num2","block8num3","block3num1","block3num4","block3num7","block6num1","block6num4","block6num7"];
+var block9num2 = ["block7num1","block7num2","block7num3","block8num1","block8num2","block8num3","block9num1","block3num2","block3num5","block3num8","block6num2","block6num5","block6num8"];
+var block9num3 = ["block7num1","block7num2","block7num3","block8num1","block8num2","block8num3","block9num1","block9num2","block3num3","block3num6","block3num9","block6num3","block6num6","block6num9"];
+//step nine
+var block7num4 = ["block1num1","block1num4","block1num7","block4num1","block4num4","block4num7","block7num1"];
+var block7num5 = ["block7num4","block1num2","block1num5","block1num8","block4num2","block4num5","block4num8","block7num2"];
+var block7num6 = ["block7num4","block7num5","block1num3","block1num6","block1num9","block4num3","block4num6","block4num9","block7num3"];
+var block8num4 = ["block7num4","block7num5","block7num6","block2num1","block2num4","block2num7","block5num1","block5num4","block5num7","block8num1"];
+var block8num5 = ["block7num4","block7num5","block7num6","block8num4","block2num2","block2num5","block2num8","block5num2","block5num5","block5num8","block8num2"];
+var block8num6 = ["block7num4","block7num5","block7num6","block8num4","block8num5","block2num3","block2num6","block2num9","block5num3","block5num6","block5num9","block8num3"];
+var block9num4 = ["block7num4","block7num5","block7num6","block8num4","block8num5","block8num6","block3num1","block3num4","block3num7","block6num1","block6num4","block6num7","block9num1"];
+var block9num5 = ["block7num4","block7num5","block7num6","block8num4","block8num5","block8num6","block9num4","block3num2","block3num5","block3num8","block6num2","block6num5","block6num8","block9num2"];
+var block9num6 = ["block7num4","block7num5","block7num6","block8num4","block8num5","block8num6","block9num4","block9num5","block3num3","block3num6","block3num9","block6num3","block6num6","block6num9","block9num3"];
+//step ten
+var block7num7 = ["block1num1","block1num4","block1num7","block4num1","block4num4","block4num7","block7num1","block7num4"];
+var block7num8 = ["block7num7","block1num2","block1num5","block1num8","block4num2","block4num5","block4num8","block7num2","block7num5"];
+var block7num9 = ["block7num7","block7num8","block1num3","block1num6","block1num9","block4num3","block4num6","block4num9","block7num3","block7num6"];
+var block8num7 = ["block7num7","block7num8","block7num9","block2num1","block2num4","block2num7","block5num1","block5num4","block5num7","block8num1","block8num4"];
+var block8num8 = ["block7num7","block7num8","block7num9","block8num7","block2num2","block2num5","block2num8","block5num2","block5num5","block5num8","block8num2","block8num5"];
+var block8num9 = ["block7num7","block7num8","block7num9","block8num7","block8num8","block2num3","block2num6","block2num9","block5num3","block5num6","block5num9","block8num3","block8num6"];
+var block9num7 = ["block7num7","block7num8","block7num9","block8num7","block8num8","block8num9","block3num1","block3num4","block3num7","block6num1","block6num4","block6num7","block9num1","block9num4"];
+var block9num8 = ["block7num7","block7num8","block7num9","block8num7","block8num8","block8num9","block9num7","block3num2","block3num5","block3num8","block6num2","block6num5","block6num8","block9num2","block9num5"];
+var block9num9 = ["block7num7","block7num8","block7num9","block8num7","block8num8","block8num9","block9num7","block9num8","block3num3","block3num6","block3num9","block6num3","block6num6","block6num9","block9num3","block9num6"];
 
-function FillRestOfBoard(data, name){
+function FillRestOfBoard(data, name, lastRow){
     //var usedNumbers = [Number(document.getElementById('block1num1').innerHTML),Number(document.getElementById('block1num2').innerHTML),Number(document.getElementById('block1num3').innerHTML)]
 var usedNumbers = [];
     for(var a = 0; a < data.length; a++){
         usedNumbers.push(Number(document.getElementById(data[a]).innerHTML));
 }
+if(lastRow) {
+    for (var u = 0; u < lastRow.length; u++) {
+        if (usedNumbers.indexOf(lastRow[u]) === -1) {
+            console.log("lastRow " + lastRow);
+          //  usedNumbers.push(lastRow[u]);
+            return document.getElementById(name).innerHTML = lastRow[u].toString();
+        }
+    }
+}
     for(var i = 1; i <= 9; i++){
         if(usedNumbers.indexOf(i) === -1){
             //usedNumbers.push(i);
-            console.log(usedNumbers);
             return document.getElementById(name).innerHTML = i.toString();
         }
         }
-
 }
-//step one - filling row one
+//step two - filling row one
 FillRestOfBoard(block2num1, "block2num1");
 FillRestOfBoard(block2num2, "block2num2");
 FillRestOfBoard(block2num3, "block2num3");
 FillRestOfBoard(block3num1, "block3num1");
 FillRestOfBoard(block3num2, "block3num2");
 FillRestOfBoard(block3num3, "block3num3");
-//step two - filling row two
-FillRestOfBoard(block2num4, "block2num4");
-FillRestOfBoard(block2num5, "block2num5");
-FillRestOfBoard(block2num6, "block2num6");
-FillRestOfBoard(block3num4, "block3num4");
-FillRestOfBoard(block3num5, "block3num5");
-FillRestOfBoard(block3num6, "block3num6");
-//step three - filling row three
-FillRestOfBoard(block2num7, "block2num7");
-FillRestOfBoard(block2num8, "block2num8");
-FillRestOfBoard(block2num9, "block2num9");
-FillRestOfBoard(block3num7, "block3num7");
-FillRestOfBoard(block3num8, "block3num8");
-FillRestOfBoard(block3num9, "block3num9");
+//step three - filling row two
+var row2 = [Number(document.getElementById("block3num3").innerHTML)];
+FillRestOfBoard(block2num4, "block2num4", row2);
+FillRestOfBoard(block2num5, "block2num5", row2);
+FillRestOfBoard(block2num6, "block2num6", row2);
+FillRestOfBoard(block3num4, "block3num4", row2);
+FillRestOfBoard(block3num5, "block3num5", row2);
+FillRestOfBoard(block3num6, "block3num6", row2);
+//step four - filling row three
+var row3 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML)];
+FillRestOfBoard(block2num7, "block2num7", row3);
+FillRestOfBoard(block2num8, "block2num8", row3);
+FillRestOfBoard(block2num9, "block2num9", row3);
+FillRestOfBoard(block3num7, "block3num7", row3);
+FillRestOfBoard(block3num8, "block3num8", row3);
+FillRestOfBoard(block3num9, "block3num9", row3);
 //step five - filling row four
-FillRestOfBoard(block4num1, "block4num1");
-FillRestOfBoard(block4num2, "block4num2");
-FillRestOfBoard(block4num3, "block4num3");
-FillRestOfBoard(block5num1, "block5num1");
-FillRestOfBoard(block5num2, "block5num2");
-FillRestOfBoard(block5num3, "block5num3");
-FillRestOfBoard(block6num1, "block6num1");
-FillRestOfBoard(block6num2, "block6num2");
-FillRestOfBoard(block6num3, "block6num3");
+var row4 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML)];
+FillRestOfBoard(block4num1, "block4num1", row4);
+FillRestOfBoard(block4num2, "block4num2", row4);
+FillRestOfBoard(block4num3, "block4num3", row4);
+FillRestOfBoard(block5num1, "block5num1", row4);
+FillRestOfBoard(block5num2, "block5num2", row4);
+FillRestOfBoard(block5num3, "block5num3", row4);
+FillRestOfBoard(block6num1, "block6num1", row4);
+FillRestOfBoard(block6num2, "block6num2", row4);
+FillRestOfBoard(block6num3, "block6num3", row4);
+//step six - filling row five
+var row5 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML)];
+FillRestOfBoard(block4num4,"block4num4", row5);
+FillRestOfBoard(block4num5,"block4num5", row5);
+FillRestOfBoard(block4num6,"block4num6", row5);
+FillRestOfBoard(block5num4,"block5num4", row5);
+FillRestOfBoard(block5num5,"block5num5", row5);
+FillRestOfBoard(block5num6,"block5num6", row5);
+FillRestOfBoard(block6num4,"block6num4", row5);
+FillRestOfBoard(block6num5,"block6num5", row5);
+FillRestOfBoard(block6num6,"block6num6", row5);
+//step seven - filling row six
+var row6 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML),Number(document.getElementById("block6num6").innerHTML)];
+FillRestOfBoard(block4num7,"block4num7",row6);
+FillRestOfBoard(block4num8,"block4num8",row6);
+FillRestOfBoard(block4num9,"block4num9",row6);
+FillRestOfBoard(block5num7,"block5num7",row6);
+FillRestOfBoard(block5num8,"block5num8",row6);
+FillRestOfBoard(block5num9,"block5num9",row6);
+FillRestOfBoard(block6num7,"block6num7",row6);
+FillRestOfBoard(block6num8,"block6num8",row6);
+FillRestOfBoard(block6num9,"block6num9",row6);
+//step eight - filling row seven
+var row7 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML),Number(document.getElementById("block6num6").innerHTML),Number(document.getElementById("block6num9").innerHTML)];
+FillRestOfBoard(block7num1,"block7num1",row7);
+FillRestOfBoard(block7num2,"block7num2",row7);
+FillRestOfBoard(block7num3,"block7num3",row7);
+FillRestOfBoard(block8num1,"block8num1",row7);
+FillRestOfBoard(block8num2,"block8num2",row7);
+FillRestOfBoard(block8num3,"block8num3",row7);
+FillRestOfBoard(block9num1,"block9num1",row7);
+FillRestOfBoard(block9num2,"block9num2",row7);
+FillRestOfBoard(block9num3,"block9num3",row7);
+//step nine - filling row eight
+var row8 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML),Number(document.getElementById("block6num6").innerHTML),Number(document.getElementById("block6num9").innerHTML),Number(document.getElementById("block9num3").innerHTML)];
+FillRestOfBoard(block7num4,"block7num4",row8);
+FillRestOfBoard(block7num5,"block7num5",row8);
+FillRestOfBoard(block7num6,"block7num6",row8);
+FillRestOfBoard(block8num4,"block8num4",row8);
+FillRestOfBoard(block8num5,"block8num5",row8);
+FillRestOfBoard(block8num6,"block8num6",row8);
+FillRestOfBoard(block9num4,"block9num4",row8);
+FillRestOfBoard(block9num5,"block9num5",row8);
+FillRestOfBoard(block9num6,"block9num6",row8);
+//step ten - filling row nine
+var row9 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML),Number(document.getElementById("block6num6").innerHTML),Number(document.getElementById("block6num9").innerHTML),Number(document.getElementById("block9num3").innerHTML),Number(document.getElementById("block9num6").innerHTML)];
+FillRestOfBoard(block7num7,"block7num7",row9);
+FillRestOfBoard(block7num8,"block7num8",row9);
+FillRestOfBoard(block7num9,"block7num9",row9);
+FillRestOfBoard(block8num7,"block8num7",row9);
+FillRestOfBoard(block8num8,"block8num8",row9);
+FillRestOfBoard(block8num9,"block8num9",row9);
+FillRestOfBoard(block9num7,"block9num7",row9);
+FillRestOfBoard(block9num8,"block9num8",row9);
+FillRestOfBoard(block9num9,"block9num9",row9);
 
 
 function shuffleArray(array) {
