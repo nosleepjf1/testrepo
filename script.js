f0a9eab8fbed88d23c9de064d3cee4ee79df86fa
jonny fewkes <jonnyfewkes@jonnys-Mac-mini.local>
2017-09-21 12:41:50 -0600

    Defeated! Didn't realize how complex sudoku algorythms are and can't justify the time to learn them. Changing gears now

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ var block9num7 = ["block7num7","block7num8","block7num9","block8num7","block8num
 var block9num8 = ["block7num7","block7num8","block7num9","block8num7","block8num8","block8num9","block9num7","block3num2","block3num5","block3num8","block6num2","block6num5","block6num8","block9num2","block9num5","block7num5","block9num1","block9num3","block9num4","block9num6"];
 var block9num9 = ["block7num7","block7num8","block7num9","block8num7","block8num8","block8num9","block9num7","block9num8","block3num3","block3num6","block3num9","block6num3","block6num6","block6num9","block9num3","block9num6","block9num1","block9num2","block9num4","block9num5"];
 
+var arrayOfXs = [];
 function FillRestOfBoard(data, name, lastRow){
     //var usedNumbers = [Number(document.getElementById('block1num1').innerHTML),Number(document.getElementById('block1num2').innerHTML),Number(document.getElementById('block1num3').innerHTML)]
 var usedNumbers = [];
@@ -118,6 +119,13 @@ if(lastRow) {
             return document.getElementById(name).innerHTML = i.toString();
         }
         }
+        arrayOfXs.push(name);
+//use the id of 'name' to find out the block number
+    //get the numbers of the rest of the block in order to find out which number can still be used
+    //add that number to the element
+    //its possible to have more than one error on the same block so i have to code for that
+    //save these positions somewhere so that later i can save them as the correct markers
+
 }
 //step two - filling row one
 FillRestOfBoard(block2num1, "block2num1");
@@ -136,78 +144,78 @@ FillRestOfBoard(block3num5, "block3num5", row2);
 FillRestOfBoard(block3num6, "block3num6", row2);
 //step four - filling row three
 var row3 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML)];
-FillRestOfBoard(block2num7, "block2num7", row3);
-FillRestOfBoard(block2num8, "block2num8", row3);
-FillRestOfBoard(block2num9, "block2num9", row3);
-FillRestOfBoard(block3num7, "block3num7", row3);
-FillRestOfBoard(block3num8, "block3num8", row3);
-FillRestOfBoard(block3num9, "block3num9", row3);
+FillRestOfBoard(block2num7, "block2num7", row2);
+FillRestOfBoard(block2num8, "block2num8", row2);
+FillRestOfBoard(block2num9, "block2num9", row2);
+FillRestOfBoard(block3num7, "block3num7", row2);
+FillRestOfBoard(block3num8, "block3num8", row2);
+FillRestOfBoard(block3num9, "block3num9", row2);
 //step five - filling row four
 var row4 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML)];
-FillRestOfBoard(block4num1, "block4num1", row4);
-FillRestOfBoard(block4num2, "block4num2", row4);
-FillRestOfBoard(block4num3, "block4num3", row4);
-FillRestOfBoard(block5num1, "block5num1", row4);
-FillRestOfBoard(block5num2, "block5num2", row4);
-FillRestOfBoard(block5num3, "block5num3", row4);
-FillRestOfBoard(block6num1, "block6num1", row4);
-FillRestOfBoard(block6num2, "block6num2", row4);
-FillRestOfBoard(block6num3, "block6num3", row4);
+FillRestOfBoard(block4num1, "block4num1", row2);
+FillRestOfBoard(block4num2, "block4num2", row2);
+FillRestOfBoard(block4num3, "block4num3", row2);
+FillRestOfBoard(block5num1, "block5num1", row2);
+FillRestOfBoard(block5num2, "block5num2", row2);
+FillRestOfBoard(block5num3, "block5num3", row2);
+FillRestOfBoard(block6num1, "block6num1", row2);
+FillRestOfBoard(block6num2, "block6num2", row2);
+FillRestOfBoard(block6num3, "block6num3", row2);
 //step six - filling row five
 var row5 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML)];
-FillRestOfBoard(block4num4,"block4num4", row5);
-FillRestOfBoard(block4num5,"block4num5", row5);
-FillRestOfBoard(block4num6,"block4num6", row5);
-FillRestOfBoard(block5num4,"block5num4", row5);
-FillRestOfBoard(block5num5,"block5num5", row5);
-FillRestOfBoard(block5num6,"block5num6", row5);
-FillRestOfBoard(block6num4,"block6num4", row5);
-FillRestOfBoard(block6num5,"block6num5", row5);
-FillRestOfBoard(block6num6,"block6num6", row5);
+FillRestOfBoard(block4num4,"block4num4", row2);
+FillRestOfBoard(block4num5,"block4num5", row2);
+FillRestOfBoard(block4num6,"block4num6", row2);
+FillRestOfBoard(block5num4,"block5num4", row2);
+FillRestOfBoard(block5num5,"block5num5", row2);
+FillRestOfBoard(block5num6,"block5num6", row2);
+FillRestOfBoard(block6num4,"block6num4", row2);
+FillRestOfBoard(block6num5,"block6num5", row2);
+FillRestOfBoard(block6num6,"block6num6", row2);
 //step seven - filling row six
 var row6 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML),Number(document.getElementById("block6num6").innerHTML)];
-FillRestOfBoard(block4num7,"block4num7",row6);
-FillRestOfBoard(block4num8,"block4num8",row6);
-FillRestOfBoard(block4num9,"block4num9",row6);
-FillRestOfBoard(block5num7,"block5num7",row6);
-FillRestOfBoard(block5num8,"block5num8",row6);
-FillRestOfBoard(block5num9,"block5num9",row6);
-FillRestOfBoard(block6num7,"block6num7",row6);
-FillRestOfBoard(block6num8,"block6num8",row6);
-FillRestOfBoard(block6num9,"block6num9",row6);
+FillRestOfBoard(block4num7,"block4num7",row2);
+FillRestOfBoard(block4num8,"block4num8",row2);
+FillRestOfBoard(block4num9,"block4num9",row2);
+FillRestOfBoard(block5num7,"block5num7",row2);
+FillRestOfBoard(block5num8,"block5num8",row2);
+FillRestOfBoard(block5num9,"block5num9",row2);
+FillRestOfBoard(block6num7,"block6num7",row2);
+FillRestOfBoard(block6num8,"block6num8",row2);
+FillRestOfBoard(block6num9,"block6num9",row2);
 //step eight - filling row seven
 var row7 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML),Number(document.getElementById("block6num6").innerHTML),Number(document.getElementById("block6num9").innerHTML)];
-FillRestOfBoard(block7num1,"block7num1",row7);
-FillRestOfBoard(block7num2,"block7num2",row7);
-FillRestOfBoard(block7num3,"block7num3",row7);
-FillRestOfBoard(block8num1,"block8num1",row7);
-FillRestOfBoard(block8num2,"block8num2",row7);
-FillRestOfBoard(block8num3,"block8num3",row7);
-FillRestOfBoard(block9num1,"block9num1",row7);
-FillRestOfBoard(block9num2,"block9num2",row7);
-FillRestOfBoard(block9num3,"block9num3",row7);
+FillRestOfBoard(block7num1,"block7num1",row2);
+FillRestOfBoard(block7num2,"block7num2",row2);
+FillRestOfBoard(block7num3,"block7num3",row2);
+FillRestOfBoard(block8num1,"block8num1",row2);
+FillRestOfBoard(block8num2,"block8num2",row2);
+FillRestOfBoard(block8num3,"block8num3",row2);
+FillRestOfBoard(block9num1,"block9num1",row2);
+FillRestOfBoard(block9num2,"block9num2",row2);
+FillRestOfBoard(block9num3,"block9num3",row2);
 //step nine - filling row eight
 var row8 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML),Number(document.getElementById("block6num6").innerHTML),Number(document.getElementById("block6num9").innerHTML),Number(document.getElementById("block9num3").innerHTML)];
-FillRestOfBoard(block7num4,"block7num4",row8);
-FillRestOfBoard(block7num5,"block7num5",row8);
-FillRestOfBoard(block7num6,"block7num6",row8);
-FillRestOfBoard(block8num4,"block8num4",row8);
-FillRestOfBoard(block8num5,"block8num5",row8);
-FillRestOfBoard(block8num6,"block8num6",row8);
-FillRestOfBoard(block9num4,"block9num4",row8);
-FillRestOfBoard(block9num5,"block9num5",row8);
-FillRestOfBoard(block9num6,"block9num6",row8);
+FillRestOfBoard(block7num4,"block7num4",row2);
+FillRestOfBoard(block7num5,"block7num5",row2);
+FillRestOfBoard(block7num6,"block7num6",row2);
+FillRestOfBoard(block8num4,"block8num4",row2);
+FillRestOfBoard(block8num5,"block8num5",row2);
+FillRestOfBoard(block8num6,"block8num6",row2);
+FillRestOfBoard(block9num4,"block9num4",row2);
+FillRestOfBoard(block9num5,"block9num5",row2);
+FillRestOfBoard(block9num6,"block9num6",row2);
 //step ten - filling row nine
 var row9 = [Number(document.getElementById("block3num3").innerHTML),Number(document.getElementById("block3num6").innerHTML),Number(document.getElementById("block3num9").innerHTML),Number(document.getElementById("block6num3").innerHTML),Number(document.getElementById("block6num6").innerHTML),Number(document.getElementById("block6num9").innerHTML),Number(document.getElementById("block9num3").innerHTML),Number(document.getElementById("block9num6").innerHTML)];
-FillRestOfBoard(block7num7,"block7num7",row9);
-FillRestOfBoard(block7num8,"block7num8",row9);
-FillRestOfBoard(block7num9,"block7num9",row9);
-FillRestOfBoard(block8num7,"block8num7",row9);
-FillRestOfBoard(block8num8,"block8num8",row9);
-FillRestOfBoard(block8num9,"block8num9",row9);
-FillRestOfBoard(block9num7,"block9num7",row9);
-FillRestOfBoard(block9num8,"block9num8",row9);
-FillRestOfBoard(block9num9,"block9num9",row9);
+FillRestOfBoard(block7num7,"block7num7",row2);
+FillRestOfBoard(block7num8,"block7num8",row2);
+FillRestOfBoard(block7num9,"block7num9",row2);
+FillRestOfBoard(block8num7,"block8num7",row2);
+FillRestOfBoard(block8num8,"block8num8",row2);
+FillRestOfBoard(block8num9,"block8num9",row2);
+FillRestOfBoard(block9num7,"block9num7",row2);
+FillRestOfBoard(block9num8,"block9num8",row2);
+FillRestOfBoard(block9num9,"block9num9",row2);
 
 
 function shuffleArray(array) {
@@ -218,4 +226,8 @@ function shuffleArray(array) {
         array[j] = temp;
     }
     return array;
+}
+
+function clicked (id){
+    console.log(id);
 }
